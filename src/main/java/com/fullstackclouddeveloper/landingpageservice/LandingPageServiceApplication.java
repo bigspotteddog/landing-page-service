eeabdd569d22c7581e0bfaac889f079a51a21bdf
bigspotteddog <bink.lynch@gmail.com>
2021-02-07 23:03:00 -0800

    Clean up.

--- src/main/java/com/fullstackclouddeveloper/landingpageservice/LandingPageServiceApplication.java
+++ src/main/java/com/fullstackclouddeveloper/landingpageservice/LandingPageServiceApplication.java
@@ -25,7 +25,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController   ;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
